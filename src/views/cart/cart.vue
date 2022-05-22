d9aa326525307cef6429d1c4dbd922d24fb25ebf
QuadnucYard <2380433991@qq.com>
2022-05-22 22:07:28 +0800

    完善购物车样式

--- src/views/cart/cart.vue
+++ src/views/cart/cart.vue
@@ -1,24 +1,31 @@
 <template>
-  <div>
+  <div style="width: 800px; margin: auto; padding: 2em 0; line-height: 2em">
     <div>
       <template v-for="item in list" :key="item.id">
-        <el-card>
-          <div>{{ item.name }}</div>
-          <div>{{ item.price }}</div>
-          <div>{{ item.date }}</div>
-          <div>{{ item.book }}</div>
+        <el-card style="margin: 1em">
+          <div style="font-weight: bolder">
+            <span>{{ item.name }}</span>
+            <span style="float: right; margin: 0 1em">{{ formatDate(item.date) }}</span>
+            <span style="float: right">￥{{ item.price }}</span>
+          </div>
+          <div style="padding: 1em">
+            <div class="tag">{{ item.book.title }}</div>
+            <div class="tag">{{ item.book.author }}</div>
+            <div class="tag">{{ item.book.publisher }}</div>
+          </div>
         </el-card>
       </template>
     </div>
     <div class="bottom-box">
       <el-card>
-        <el-button type="primary">清空购物车</el-button>
+        <el-button type="primary">结算</el-button>
       </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import moment from "moment";
 import { getCart } from "@/service/cart.js";
 
 export default {
@@ -37,6 +44,9 @@ export default {
       } = await getCart({ uid: this.$store.state.user.uid });
       this.list = list.goods;
     },
+    formatDate(date) {
+      return date == null ? "" : moment(date).format("YYYY/MM/DD");
+    },
   },
 };
 </script>
@@ -49,5 +59,13 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.tag {
+  display: inline-block;
+  margin: 0 5px;
+  padding: 5px 10px;
+  border-radius: 2px;
+  background-color: powderblue;
 }
 </style>
